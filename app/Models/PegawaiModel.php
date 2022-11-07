4644c51fa73cffec696b24967eb35c19c17a2134
MuhamadIqbal-12210257 <bayung912@gmail.com>
2022-11-07 20:03:21 +0700

    commit iqbal

---
+++ app/Models/PegawaiModel.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PegawaiModel extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'pegawai';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = false;
+    protected $allowedFields    = [];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+
+    static function view(){
+        return (new PegawaiModel())
+                ->join('bagian', 'bagian.id=bagian_id')
+                ->select('pegawai.*, bagian.nama');
+    }
+}
